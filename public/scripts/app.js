e295b2d54a43582354ce53ec283b5017545456fc
Nicolas Magana <nmagana@lyft.com>
2018-07-23 00:28:40 -0700

    Created src file to write JSX code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,13 @@
+'use strict';
+
 console.log('App.js is running!');
 
 // JSX -JavaScript XML
-// var template = <h1> Hello there </h1>
-
 var template = React.createElement(
-    "p",
-    null,
-    "This is JSX from app.js!"
-  );
-
+  'h1',
+  null,
+  ' Hello there '
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
